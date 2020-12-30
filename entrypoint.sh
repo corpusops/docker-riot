7ddd6d5dbfc33a490d9894bc786c8ac899413676
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2020-12-30 10:29:41 +0100

    Upgrade to 1.7.16 & nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,13 @@
 #!/bin/sh
 set -ex
-DOMAINS="${DOMAINS:-localhost}"
-DEFAULT_HS_URL="${DEFAULT_HS_URL-}"
-DEFAULT_IS_URL="${DEFAULT_IS_URL-}"
-BRAND="${BRAND:-Riot}"
-INTEGRATIONS_UI_URL="${INTEGRATIONS_UI_URL-}"
-INTEGRATIONS_REST_URL="${INTEGRATIONS_REST_URL-}"
-INTEGRATIONS_WIDGETS_URL="${INTEGRATIONS_WIDGETS_URL-}"
-INTEGRATIONS_WIDGETSJITSI_URL="${INTEGRATIONS_WIDGETSJITSI_URL-"[]"}"
-DEFAULT_THEME=${DEFAULT_THEME:-dark}
 p=/riot-web
-cfg=$p/config.json
-set -eux
-sed -i "s#{{BUG_URL}}#${BUG_URL}#" $cfg
-sed -i "s#{{DEFAULT_HS_URL}}#${DEFAULT_HS_URL}#" $cfg
-sed -i "s#{{DEFAULT_IS_URL}}#${DEFAULT_IS_URL}#" $cfg
-sed -i "s#{{INTEGRATIONS_UI_URL}}#${INTEGRATIONS_UI_URL}#" $cfg
-sed -i "s#{{INTEGRATIONS_REST_URL}}#${INTEGRATIONS_REST_URL}#" $cfg
-sed -i "s#{{INTEGRATIONS_WIDGETSJITSI_URL}}#${INTEGRATIONS_WIDGETSJITSI_URL}#" $cfg
-sed -i "s#{{INTEGRATIONS_WIDGETS_URL}}#${INTEGRATIONS_WIDGETS_URL}#" $cfg
-sed -i "s#{{DEFAULT_THEME}}#${DEFAULT_THEME}#" $cfg
-sed -i "s#{{BRAND}}#${BRAND}#" $cfg
-for d in ${DOMAINS};do
-    cp -f $cfg $p/config.${d}.json
-done
-cd $p
-exec http-server -p 8080 -A 0.0.0.0 -c 3500
+TVHOST=${TVHOST:-/etc/nginx.conf}
+VHOST=${VHOST:-/etc/nginx/conf.d/default.conf}
+CFG=${CFG:-$p/config.json}
+TCFG=${TCFG:-$CFG}
+if [ "x$RENDER_CFG" = "x1" ];then frep $TCFG:$CFG --overwrite;fi
+if [ "x$TVHOST" != "x" ];then frep $TVHOST:$VHOST --overwrite;fi
+sed -i -re "s/user\s+.*;/user $NGINX_USER;/g" /etc/nginx/nginx.conf
+for d in ${DOMAINS};do cp -f $CFG $p/config.${d}.json;done
+export SUPERVISORD_CONFIGS=nginx cron rsyslog
+exec /bin/supervisord.sh
